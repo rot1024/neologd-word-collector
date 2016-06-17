5c3cc0c87ff249d4671dcc8f32904212b26c8bc7
rot1024 <rot1024@gmail.com>
2016-06-17 18:21:35 +0900

    Add all command

--- bin/index.js
+++ bin/index.js
@@ -11,6 +11,74 @@ const co = require("co");
 
 const collector = require("..");
 
+function scrape(args) {
+  return co(function *() {
+
+    console.log("Start scraping");
+
+    const resumeFile = path.resolve(__dirname, "..", "cache", "resume.json");
+    const bar = new ProgressBar("[:bar] :i :c :current/:total :percent :eta s", { total: 1, width: 20 });
+
+    const stream = fs.createWriteStream(args.output, { flags: "a" });
+
+    let resume, firstCharIndex, firstPageIndex;
+    try {
+      resume = JSON.parse(fs.readFileSync(resumeFile, "utf8"));
+      firstCharIndex = resume.page < resume.max ? resume.charIndex : resume.charIndex + 1;
+      firstPageIndex = resume.page < resume.max ? resume.page : 0;
+    } catch (e) {
+      resume = {};
+      firstCharIndex = args.char;
+      firstPageIndex = args.pageIndex;
+    }
+
+    try {
+      yield collector.scraper.scrapeAll({
+        progressCb(c, i, p, m, w) {
+          stream.write(w.map(ww => ww.name + "\t" + ww.yomi).join(os.EOL) + os.EOL);
+          fs.writeFile(resumeFile, JSON.stringify({ char: c, charIndex: i, page: p, max: m }));
+          bar.total = m;
+          bar.tick(p - bar.curr, { c, i });
+        },
+        firstCharIndex,
+        firstPageIndex,
+        concurrency: 1,
+        concurrencyForEachCharacter: 1,
+        waitMillisec: 1000
+      });
+
+      console.log("DONE!");
+    } catch (err) {
+      console.error(os.EOL + os.EOL + "ERROR!");
+      if (err.error && err.error.statusCode) {
+        console.log("FetchError! Status code: " + err.error.statusCode);
+      } else {
+        console.error(err.stack || err.message || err);
+      }
+    } finally {
+      stream.end();
+    }
+
+  });
+}
+
+function drop(args) {
+  return co(function *() {
+
+    console.log("Loading dictionary...");
+
+    const set = yield collector.dropper.createDictionarySet(args.dictionary);
+
+    console.log("Dropping words...");
+
+    const count = yield collector.dropper.drop(args.input, set, args.output);
+
+    console.log("DONE! " + count + " words are recorded!");
+
+  });
+}
+
+
 const argv = yargs
 
   .command("scrape", "Scrape data from nicodic", {
@@ -24,68 +92,28 @@ const argv = yargs
       default: 0,
       type: "number",
       description: "Specify start page index"
+    },
+    output: {
+      alias: "o",
+      default: "cache/words.txt",
+      type: "string",
+      description: "Specify output file path"
     }
   }, args => {
-
-    console.log("Start scraping");
-
-    const resumeFile = path.resolve(__dirname, "..", "cache", "resume.json");
-    const bar = new ProgressBar("[:bar] :i :c :current/:total :percent :eta s", { total: 1, width: 20 });
-
-    const stream = fs.createWriteStream(
-      path.resolve(__dirname, "..", "cache", "words.txt"),
-      { flags: "a" }
-    );
-
-    let resume, firstCharIndex, firstPageIndex;
-    try {
-      resume = JSON.parse(fs.readFileSync(resumeFile, "utf8"));
-      firstCharIndex = resume.page < resume.max ? resume.charIndex : resume.charIndex + 1;
-      firstPageIndex = resume.page < resume.max ? resume.page : 0;
-    } catch (e) {
-      resume = {};
-      firstCharIndex = args.char;
-      firstPageIndex = args.pageIndex;
-    }
-
-    collector.scraper.scrapeAll({
-      progressCb(c, i, p, m, w) {
-        stream.write(w.map(ww => ww.name + "\t" + ww.yomi).join(os.EOL) + os.EOL);
-        fs.writeFile(resumeFile, JSON.stringify({ char: c, charIndex: i, page: p, max: m }));
-        bar.total = m;
-        bar.tick(p - bar.curr, { c, i });
-      },
-      firstCharIndex,
-      firstPageIndex,
-      concurrency: 1,
-      concurrencyForEachCharacter: 1,
-      waitMillisec: 1000
-    }).then(() => {
-      stream.end();
-      console.log("DONE!");
-    }).catch(err => {
-      stream.end();
-      console.error(os.EOL + os.EOL + "ERROR!");
-      if (err.error && err.error.statusCode) {
-        console.log("FetchError! Status code: " + err.error.statusCode);
-      } else {
-        console.error(err.stack || err.message || err);
-      }
+    scrape(args).catch(err => {
+      console.error(err.stack || err);
     });
-
   })
 
   .command("drop", "Drop words recorded in dictionary and write results to file", {
     input: {
       alias: "i",
       type: "string",
-      required: true,
       default: "cache/words.txt"
     },
     output: {
       alias: "o",
       type: "string",
-      required: true,
       default: "cache/results.txt"
     },
     dictionary: {
@@ -94,23 +122,33 @@ const argv = yargs
       required: true
     }
   }, args => {
-
-    co(function *() {
-
-      console.log("Loading dictionary...");
-
-      const set = yield collector.dropper.createDictionarySet(args.dictionary);
-
-      console.log("Dropping words...");
-
-      const count = yield collector.dropper.drop(args.input, set, args.output);
-
-      console.log("DONE! " + count + " words are recorded!");
-
-    }).catch(err => {
+    drop(args).catch(err => {
       console.error(err.stack || err);
     });
+  })
 
+  .command("all", "Exec scrape command and then drop command", {
+    words: {
+      alias: "w",
+      type: "string",
+      default: "cache/words.txt"
+    },
+    output: {
+      alias: "o",
+      type: "string",
+      default: "cache/results.txt"
+    },
+    dictionary: {
+      alias: "d",
+      type: "string",
+      required: true
+    }
+  }, args => {
+    scrape({ output: args.words })
+    .then(() => drop({ input: args.words, output: args.output, dictionary: args.dictionary }))
+    .catch(err => {
+      console.error(err.stack || err);
+    });
   })
 
   .help()
